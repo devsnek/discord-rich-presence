77ed7e5eb8baf73f7a9e42e57c6018a0cf230529
Gus Caplan <me@gus.host>
2017-12-08 17:39:28 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const Discord = require('discord-rpc');
+const EventEmitter = require('events');
+
+function makeClient(id) {
+  const rpc = new Discord.Client({ transport: 'ipc' });
+
+  let connected = false;
+  let activityCache = null;
+
+  const instance = new class RP extends EventEmitter {
+    updatePresence(d) {
+      if (connected)
+        rpc.setActivity(d).catch((e) => this.emit('error', e));
+      else
+        activityCache = d;
+    }
+
+    reply(user, response) {
+      const handle = (e) => this.emit('error', e);
+      switch (response) {
+        case 'YES':
+          rpc.sendJoinInvite(user).catch(handle);
+          break;
+        case 'NO':
+        case 'IGNORE':
+          rpc.closeJoinRequest(user).catch(handle);
+      }
+    }
+
+    disconnect() {
+      rpc.destroy().catch((e) => this.emit('error', e));
+    }
+  };
+
+  rpc.login(id)
+    .then(() => {
+      instance.emit('connected');
+      connected = true;
+      if (activityCache) {
+        rpc.setActivity(activityCache).catch((e) => instance.emit('error', e));
+        activityCache = null;
+      }
+      rpc.subscribe('ACTIVITY_JOIN', ({ secret }) => {
+        instance.emit('join', secret);
+      });
+      rpc.subscribe('ACTIVITY_SPECTATE', ({ secret }) => {
+        instance.emit('spectate', secret);
+      });
+      rpc.subscribe('ACTIVITY_JOIN_REQUEST', (user) => {
+        instance.emit('joinRequest', user);
+      });
+    })
+    .catch((e) => instance.emit('error', e));
+
+  return instance;
+}
+
+module.exports = makeClient;
---
+++ test.js
@@ -0,0 +1,38 @@
+const client = require('.')('180984871685062656');
+
+client.on('join', (secret) => {
+  console.log('we should join with', secret);
+});
+
+client.on('spectate', (secret) => {
+  console.log('we should spectate with', secret);
+});
+
+client.on('joinRequest', (user) => {
+  if (user.discriminator === '1337')
+    client.reply(user, 'YES');
+  else
+    client.reply(user, 'IGNORE');
+});
+
+client.on('connected', () => {
+  console.log('connected!');
+
+  client.updatePresence({
+    state: 'slithering',
+    details: '🐍',
+    startTimestamp: Date.now(),
+    endTimestamp: Date.now() + 1337,
+    largeImageKey: 'snek_large',
+    smallImageKey: 'snek_small',
+    partyId: 'snek_party',
+    partySize: 1,
+    partyMax: 1,
+    matchSecret: 'slithers',
+    joinSecret: 'boop',
+    spectateSecret: 'sniff',
+    instance: true,
+  });
+});
+
+process.on('unhandledRejection', console.error);
