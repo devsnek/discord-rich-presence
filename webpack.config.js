2476b8fd90a3a7b01a5565ba6a9b13456223f8e4
Gus Caplan <me@gus.host>
2017-12-11 20:55:21 -0600

    add webpack config cuz people aren't smart enough to figure this out themselves

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const plugins = [
+  new webpack.optimize.ModuleConcatenationPlugin(),
+];
+
+const prod = process.env.NODE_ENV === 'production';
+
+if (prod) {
+  plugins.push(new UglifyJSPlugin({
+    uglifyOptions: {
+      mangle: { keep_classnames: true },
+      output: { comments: false },
+    },
+  }));
+}
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve('.'),
+    filename: `browser${prod ? '.min' : ''}.js`,
+    library: 'DiscordRichPresence',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      { test: /\.md$/, loader: 'ignore-loader' },
+    ],
+  },
+  node: {
+    fs: 'empty',
+    dns: 'empty',
+    tls: 'empty',
+    child_process: 'empty',
+    dgram: 'empty',
+    __dirname: true,
+    process: false,
+    path: 'empty',
+    Buffer: false,
+    zlib: 'empty',
+  },
+  plugins,
+};
